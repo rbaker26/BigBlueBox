54be89211a37bff828e372413a7fc43da7d1b8af
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2017-11-17 17:28:21 -0800

    Update mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -21,27 +20,3 @@ private:
 };
 
 #endif // MAINWINDOW_H
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
-};
-
-#endif // MAINWINDOW_H
->>>>>>> 24ee9c24c6a4df44549d5ddca18b652ca9de84ae
