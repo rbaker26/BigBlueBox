a16c609c7426a5eab082edf3178339a3c86eacf3
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2017-11-17 17:27:52 -0800

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -13,19 +12,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-=======
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
->>>>>>> 24ee9c24c6a4df44549d5ddca18b652ca9de84ae
