90790c405811fa47c8f4bfb671785b92b4ed0829
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2017-11-17 17:27:26 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 #include <QApplication>
 
@@ -10,16 +9,3 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
-}
->>>>>>> 24ee9c24c6a4df44549d5ddca18b652ca9de84ae
