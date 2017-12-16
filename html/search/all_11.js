6563d982e3cab4189b88dc106353e6d5119cab95
Travis CI <travis@travis-ci.org>
2017-12-16 09:21:28 +0000

    Deploy code docs to GitHub Pages Travis build: 89
    Commit: f9a5eecf29586352135b54d2bfe82de2e55dbc7d

--- html/search/all_11.js
+++ html/search/all_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['style',['Style',['../md__home_travis_build_rbaker26__big_blue_box__style.html',1,'']]],
+  ['style_20and_20coding_20standards',['Style and Coding Standards',['../md__home_travis_build_rbaker26__big_blue_box__style.html',1,'']]],
   ['sendrowtotoolbox',['sendRowToToolBox',['../class_reports_view.html#ae67b95dd993a575d09b467f89557aae2',1,'ReportsView']]],
   ['setpassword',['setPassword',['../class_user_session.html#a2453e321df5422f34aad1abced2ecf10',1,'UserSession']]],
   ['setupui',['setupUi',['../class_ui___gear_central.html#a2d94ab5677c2d9450c9bdd30f96f68a3',1,'Ui_GearCentral::setupUi()'],['../class_ui___main_window.html#acf4a0872c4c77d8f43a2ec66ed849b58',1,'Ui_MainWindow::setupUi()'],['../class_ui___reports_view.html#aea6777c38ef5832336dad493dcf8e27b',1,'Ui_ReportsView::setupUi()']]],
--- html/search/pages_1.js
+++ html/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['style',['Style',['../md__home_travis_build_rbaker26__big_blue_box__style.html',1,'']]]
+  ['style_20and_20coding_20standards',['Style and Coding Standards',['../md__home_travis_build_rbaker26__big_blue_box__style.html',1,'']]]
 ];
