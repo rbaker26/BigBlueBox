ec21fd68040f644f92e3f88714dbbe58a16ab9c9
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2017-12-21 18:58:11 -0800

    escape chars

--- src/_regex.cpp
+++ src/_regex.cpp
@@ -11,7 +11,8 @@ _Regex::_Regex()
 //*********************************************************************************
 bool _Regex::isItemName(QString s)
 {
-    QRegularExpression rx("^[A-Za-z0-9().\-_]{1}[A-Za-z0-9() .-_]{0,35}$");
+    // need to sanitize string for spec. chars.
+    QRegularExpression rx("^[A-Za-z0-9().\-_]{1}[A-Za-z0-9() .\-_]{0,35}$");
     QRegularExpressionMatch match = rx.match(s);
     return match.hasMatch();
 }
