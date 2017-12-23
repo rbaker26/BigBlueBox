17716cc1b76d4979938a435d69d60fffd89d1b66
Travis CI <travis@travis-ci.org>
2017-12-23 00:35:13 +0000

    Deploy code docs to GitHub Pages Travis build: 174
    Commit: 541905898c16337c40acb98d06ec387510b8a231

--- html/search/all_12.js
+++ html/search/all_12.js
@@ -2,8 +2,6 @@ var searchData=
 [
   ['tarps',['TARPS',['../classbbb_1_1_category.html#ae6842fe56f139f41a9e7cf4b0a8d98daad8d6be49f49f7bd410de0e43f8e0e9cc',1,'bbb::Category']]],
   ['testcatenumvals',['testCatEnumVals',['../classbbb_test_1_1_category_test.html#a6b8e930731c0cb0b55bad0aa70c2100b',1,'bbbTest::CategoryTest']]],
-  ['testenumtostring',['testEnumToString',['../classbbb_test_1_1_category_test.html#a741dbff3267c92a4c7c608d315fbefe9',1,'bbbTest::CategoryTest']]],
-  ['testenumtostring_5fdata',['testEnumToString_data',['../classbbb_test_1_1_category_test.html#a92dd8546e58affd773c8872e89ee9f25',1,'bbbTest::CategoryTest']]],
   ['testinttostring',['testIntToString',['../classbbb_test_1_1_category_test.html#a9ee3cfeeb3ef2f0d2ba25f5e8736fd03',1,'bbbTest::CategoryTest']]],
   ['testinttostring_5fdata',['testIntToString_data',['../classbbb_test_1_1_category_test.html#ad83d42e46b3e35651c4e98a8bddf4a76',1,'bbbTest::CategoryTest']]],
   ['testisitemname',['testIsItemName',['../classbbb_test_1_1___regex_test.html#ada7fd54b12d91d356780b6ee1c9da02f',1,'bbbTest::_RegexTest']]],
--- html/search/functions_c.js
+++ html/search/functions_c.js
@@ -1,8 +1,6 @@
 var searchData=
 [
   ['testcatenumvals',['testCatEnumVals',['../classbbb_test_1_1_category_test.html#a6b8e930731c0cb0b55bad0aa70c2100b',1,'bbbTest::CategoryTest']]],
-  ['testenumtostring',['testEnumToString',['../classbbb_test_1_1_category_test.html#a741dbff3267c92a4c7c608d315fbefe9',1,'bbbTest::CategoryTest']]],
-  ['testenumtostring_5fdata',['testEnumToString_data',['../classbbb_test_1_1_category_test.html#a92dd8546e58affd773c8872e89ee9f25',1,'bbbTest::CategoryTest']]],
   ['testinttostring',['testIntToString',['../classbbb_test_1_1_category_test.html#a9ee3cfeeb3ef2f0d2ba25f5e8736fd03',1,'bbbTest::CategoryTest']]],
   ['testinttostring_5fdata',['testIntToString_data',['../classbbb_test_1_1_category_test.html#ad83d42e46b3e35651c4e98a8bddf4a76',1,'bbbTest::CategoryTest']]],
   ['testisitemname',['testIsItemName',['../classbbb_test_1_1___regex_test.html#ada7fd54b12d91d356780b6ee1c9da02f',1,'bbbTest::_RegexTest']]],
