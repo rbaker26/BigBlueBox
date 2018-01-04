24f2d779979e6af8063318d9454d867215800737
Travis CI <travis@travis-ci.org>
2018-01-04 07:45:10 +0000

    Deploy code docs to GitHub Pages Travis build: 197
    Commit: aecd5bca1a43333d0eff6869062361b3d5342144

--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -4,9 +4,7 @@ var searchData=
   ['on_5factionhome_5ftriggered',['on_actionHome_triggered',['../class_main_window.html#a313c36eed19a71e2525da1e8041eb6a0',1,'MainWindow']]],
   ['on_5fcheckbox_5feditson_5fclicked',['on_checkBox_editsOn_clicked',['../class_reports_view.html#a89b2e4e29486ddb2a1f8a129edb59367',1,'ReportsView']]],
   ['on_5fcombobox_5fadddel_5factivated',['on_comboBox_addDel_activated',['../class_reports_view.html#ada0efc617e671329badefe711a815f16',1,'ReportsView']]],
-  ['on_5fcombobox_5fadddel_5fcurrentindexchanged',['on_comboBox_addDel_currentIndexChanged',['../class_reports_view.html#a13858bc2045f7739674bf683b57f8d2f',1,'ReportsView']]],
   ['on_5fcombobox_5fcurrentindexchanged',['on_comboBox_currentIndexChanged',['../class_reports_view.html#a2f849e56c1e598de3ae680bf01a6cc96',1,'ReportsView']]],
-  ['on_5fpushbutton_5fadddelete_5fclicked',['on_pushButton_addDelete_clicked',['../class_reports_view.html#a4ff9516632dfee7b7affa091b131d151',1,'ReportsView']]],
   ['on_5fpushbutton_5fedit_5fclicked',['on_pushButton_edit_clicked',['../class_reports_view.html#a5fd0369f81da5afd34b31e35c6583a59',1,'ReportsView']]],
   ['on_5fpushbutton_5fmakereport_5fclicked',['on_pushButton_makeReport_clicked',['../class_reports_view.html#afa5f5644331a9f66811b2a21aad02b36',1,'ReportsView']]],
   ['on_5ftablewidget_5finv_5fcelldoubleclicked',['on_tableWidget_inv_cellDoubleClicked',['../class_reports_view.html#ad5cf98addd840c1e257a556598ce66d7',1,'ReportsView']]]
