b91b255e54141dab99282693050d9c1285710b02
Travis CI <travis@travis-ci.org>
2018-01-08 08:28:31 +0000

    Deploy code docs to GitHub Pages Travis build: 290
    Commit: 5fac344a1738a4adf3c35f8e4caf598ed796dca2

--- html/search/all_12.js
+++ html/search/all_12.js
@@ -4,6 +4,8 @@ var searchData=
   ['testcatenumvals',['testCatEnumVals',['../classbbb_test_1_1_category_test.html#a6b8e930731c0cb0b55bad0aa70c2100b',1,'bbbTest::CategoryTest']]],
   ['testinttostring',['testIntToString',['../classbbb_test_1_1_category_test.html#a9ee3cfeeb3ef2f0d2ba25f5e8736fd03',1,'bbbTest::CategoryTest']]],
   ['testinttostring_5fdata',['testIntToString_data',['../classbbb_test_1_1_category_test.html#ad83d42e46b3e35651c4e98a8bddf4a76',1,'bbbTest::CategoryTest']]],
+  ['testisboxname',['testIsBoxName',['../classbbb_test_1_1___regex_test.html#a42dfd7c1869bc8e626931ec5b4428e44',1,'bbbTest::_RegexTest']]],
+  ['testisboxname_5fdata',['testIsBoxName_data',['../classbbb_test_1_1___regex_test.html#a22fed0f8fa5f25105c3646d600889757',1,'bbbTest::_RegexTest']]],
   ['testisitemname',['testIsItemName',['../classbbb_test_1_1___regex_test.html#ada7fd54b12d91d356780b6ee1c9da02f',1,'bbbTest::_RegexTest']]],
   ['testisitemname_5fdata',['testIsItemName_data',['../classbbb_test_1_1___regex_test.html#af1f630ee33f580e7726b2aac98407ee6',1,'bbbTest::_RegexTest']]],
   ['teststringtoint',['testStringToInt',['../classbbb_test_1_1_category_test.html#a12940acd2aeb674e62cde75ce96aec37',1,'bbbTest::CategoryTest']]],
