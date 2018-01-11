73ed06ba6a2258332917bccb81655f76086f3043
Travis CI <travis@travis-ci.org>
2018-01-11 02:33:37 +0000

    Deploy code docs to GitHub Pages Travis build: 357
    Commit: 135f1e23fcbfa97d702adaa39a10e9644ab23e92

--- html/search/functions_c.js
+++ html/search/functions_c.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['sendrowtotoolbox',['sendRowToToolBox',['../class_reports_view.html#ae67b95dd993a575d09b467f89557aae2',1,'ReportsView']]],
   ['setdefaultreportpath',['setDefaultReportPath',['../classbbb_1_1_directory_handler.html#abe8eb160489f3565e4694e8a5d0bed1f',1,'bbb::DirectoryHandler']]],
-  ['setfilepath',['setFilePath',['../classbbb_1_1___file_writer.html#a3ac49967627875a79db97980bc6e68a2',1,'bbb::_FileWriter::setFilePath()'],['../__filewriter_8cpp.html#a0bbd8d28acc88fa8191fb3e863af8e16',1,'setFilePath():&#160;_filewriter.cpp']]],
   ['setpassword',['setPassword',['../classbbb_1_1_user_session.html#a2453e321df5422f34aad1abced2ecf10',1,'bbb::UserSession']]],
   ['settingspage',['SettingsPage',['../class_settings_page.html#a2f7b734cd4b293694140ed7504d19a55',1,'SettingsPage']]],
   ['setusername',['setUsername',['../classbbb_1_1_user_session.html#af2e2f336538818015e642545db39571a',1,'bbb::UserSession']]]
